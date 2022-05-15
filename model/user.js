adb15031d42f8099d0ac1feac0ec5ccb184a4cda
NaikyDing <74ding@gmail.com>
2022-05-15 23:54:25 +0800

    feat (model): 使用者
    - mail 加入驗証
    - 新增參數

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const { isEmail } = require('validator')
 
 const userSchema = new Schema({
   nickname: {
@@ -8,12 +9,39 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
-    require: [true, '請輸入電子信箱']
+    require: [true, '請輸入電子信箱'],
+    validate: {
+      validator (email) {
+        return isEmail(email)
+      },
+      message: props => `${props.value} 不是正確的電子信箱格式`
+    }
   },
   password: {
     type: String,
+    minlength: [8, '密碼至少 8 碼'],
     required: [true, '請輸入密碼'],
     select: false
+  },
+  avatar: {
+    type: String,
+    default: 'default Img'
+  },
+  gender: {
+    type: String,
+    enum: ['male', 'female']
+  },
+  posts: {
+    type: Array,
+    default: []
+  },
+  likes: {
+    type: Array,
+    default: []
+  },
+  follows: {
+    type: Array,
+    default: []
   },
   createdAt: {
     type: Date,
@@ -21,7 +49,11 @@ const userSchema = new Schema({
     select: false
   }
 }, {
-  versionKey: false
+  versionKey: false,
+  timestamps: {
+    createdAt: false,
+    updatedAt: true
+  }
 })
 
 const User = mongoose.model('User', userSchema)
