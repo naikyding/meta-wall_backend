31e376b54a842c7946e40d6c6ad711b7f8572955
NaikyDing <74ding@gmail.com>
2022-05-15 23:53:53 +0800

    feat (user-register): 使用者註冊功能

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,45 @@
+const User = require('../model/user')
+const { ApiError } = require('../utils/errorHandle')
+const { successResponse } = require('../utils/responseHandle')
 
 const checkToken = async (req, res, next) => {
   res.status(200).send({ status: true, user: req.user })
 }
 
-module.exports = { checkToken }
+const register = async (req, res, next) => {
+  const {
+    nickname,
+    email,
+    password,
+    passwordConfirm
+  } = req.body
+
+  // const dataAry = [
+  //   nickname,
+  //   email,
+  //   password,
+  //   passwordConfirm
+  // ]
+
+  // if (emptyInBody(dataAry)) return next(ApiError.badRequest(422, '資料錯誤，請完善必填項目'))
+  // if (!isEmail(email)) return next(ApiError.badRequest(422, '電子信箱格式錯誤!'))
+  if (!(password === passwordConfirm)) return next(ApiError.badRequest(422, '輸入密碼不一致!'))
+
+  const createdData = await User.create({
+    nickname,
+    email,
+    password,
+    passwordConfirm
+  })
+  successResponse({
+    res,
+    statusCode: 201,
+    message: '註冊成功',
+    data: {
+      nickname: createdData.nickname,
+      email: createdData.email
+    }
+  })
+}
+
+module.exports = { checkToken, register }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const { Router } = require('express')
-const { checkToken } = require('../controller/user')
 const { isAuth } = require('../utils/auth')
 const { apiCatch } = require('../utils/errorHandle')
+const { checkToken, register } = require('../controller/user')
 
 const router = Router()
 
 // Token 驗證
 router.get('/check_token', isAuth, apiCatch(checkToken))
+
+// 註冊
+router.post('/register', apiCatch(register))
 
 module.exports = router
