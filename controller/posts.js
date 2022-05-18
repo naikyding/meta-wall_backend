64479c78acc5778b10f71e07cf4fbdc8ff26cbfe
NaikyDing <74ding@gmail.com>
2022-05-18 17:55:06 +0800

    feat (posts): 取得列表 populate user

--- controller/posts.js
+++ controller/posts.js
@@ -7,7 +7,14 @@ const getPostsList = async (req, res, next) => {
   const keyword = new RegExp(q)
   const createdAtSort = (s && s === 'o') ? 1 : -1 // o: 舊到新
 
-  const postList = await Post.find({ content: keyword }).select('-updatedAt').sort({ createdAt: createdAtSort })
+  const postList = await Post.find({ content: keyword })
+    .populate({
+      path: 'user',
+      select: 'nickname avatar'
+    })
+    .select('-updatedAt')
+    .sort({ createdAt: createdAtSort })
+
   if (!postList) return next(ApiError.badRequest(undefined, '操作失敗，請重試'))
 
   successResponse({
