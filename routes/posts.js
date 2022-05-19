237c88ac38c89b7150ff8060bcd465c40b6bac60
NaikyDing <74ding@gmail.com>
2022-05-20 00:13:15 +0800

    feat (posts): 新增 auth 驗証

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@ const { Router } = require('express')
 const router = Router()
 const { apiCatch } = require('../utils/errorHandle')
 const { getPostsList } = require('../controller/posts')
+const { auth } = require('../utils/auth')
 
-router.get('/', apiCatch(getPostsList))
+router.get('/', auth, apiCatch(getPostsList))
 
 module.exports = router
