519abed72d86b00b9a972482be59eb02e52b101a
NaikyDing <74ding@gmail.com>
2022-05-24 15:02:55 +0800

    feat (model): comments

---
+++ model/comments.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const commentSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  versionKey: false,
+  timestamps: {
+    createdAt: false,
+    updatedAt: true
+  }
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment
