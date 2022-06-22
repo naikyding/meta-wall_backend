00b7ed918198f42c396def90026be4ab0618fd8f
Naiky <74ding@gmail.com>
2022-06-22 16:17:19 +0800

    feat (router): 新增 v1 路由

---
+++ routes/v1/auth.js
@@ -0,0 +1,36 @@
+const { Router } = require('express')
+const { auth } = require('../../utils/auth')
+const { apiCatch } = require('../../utils/errorHandle')
+const {
+  checkToken, register, login, forgotPassword, resetPassword,
+  authGoogle, authFacebook
+} = require('../../controller/auth')
+const passport = require('passport')
+const authFacebookMiddleWare = require('../../service/authFacebook')
+const router = Router()
+
+router.get('/check_token', auth, apiCatch(checkToken))
+
+router.post('/register', apiCatch(register))
+
+router.post('/login', apiCatch(login))
+
+router.post('/forgot-password', apiCatch(forgotPassword))
+
+router.patch('/reset_password', apiCatch(resetPassword))
+
+router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: `${process.env.APP_DOMAIN}login` }),
+  apiCatch(authGoogle)
+)
+
+router.get('/facebook', authFacebookMiddleWare.authenticate('facebook', { scope: ['email', 'public_profile'] }))
+
+router.get(
+  '/facebook/callback',
+  authFacebookMiddleWare.authenticate('facebook', { session: false, failureRedirect: `${process.env.APP_DOMAIN}login` }),
+  apiCatch(authFacebook))
+
+module.exports = router
---
+++ routes/v1/user.js
@@ -0,0 +1,23 @@
+const { Router } = require('express')
+const { auth } = require('../../utils/auth')
+const { apiCatch } = require('../../utils/errorHandle')
+const {
+  userBaseInfo, updateUserBaseInfo, updatePassword,
+  userFollows, unUserFollows
+} = require('../../controller/user')
+const postFiles = require('../../utils/formFiles')
+
+const router = Router()
+
+// 取得使用者基本資料
+router.get('/', auth, apiCatch(userBaseInfo))
+
+// 修改使用者基本資料
+router.patch('/', postFiles, auth, apiCatch(updateUserBaseInfo))
+
+// 修改使用者密碼
+router.patch('/update_password', auth, apiCatch(updatePassword))
+router.post('/:userId/follows', auth, apiCatch(userFollows))
+router.delete('/:userId/follows', auth, apiCatch(unUserFollows))
+
+module.exports = router
